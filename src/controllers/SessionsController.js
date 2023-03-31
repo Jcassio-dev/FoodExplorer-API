9f7fdfe1f8d73386c29bd8ac1065e4eea7ba7e49
José Cássio <josecassio2013@gmail.com>
2023-03-31 12:01:12 -0300

    add: SessionsController  > Create > Password  Validation

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,3 +1,4 @@
+const { compare } = require("bcryptjs");
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError")
 
@@ -8,7 +9,13 @@ class SessionsController{
         const user = await knex("users").where({email}).first();
 
         if(!user){
-            throw new AppError("E-mail e/ou senha incorreta!")
+            throw new AppError("E-mail e/ou senha incorreta!");
+        }
+        
+        const comparePassword = await compare(password, user.password);
+
+        if(!comparePassword){
+            throw new AppError("Email e/ou senha incorreta!");
         }
 
         return response.json(user)
