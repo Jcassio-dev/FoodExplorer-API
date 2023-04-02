a27c4bc7be2ae8a904dd6e06511be4964109e504
José Cássio <josecassio2013@gmail.com>
2023-04-02 00:22:48 -0300

    add: controllers > FoodAvatarController && add: patch foodAvatar

--- src/routes/foods.routes.js
+++ src/routes/foods.routes.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
+const multer = require("multer");
 
+const uploadConfig = require("../configs/upload")
 const FoodsController = require("../controllers/FoodsController");
+const FoodAvatarController = require("../controllers/FoodAvatarController");
 
 const foodsController = new FoodsController();
+const foodAvatarController = new FoodAvatarController();
 const foodsRoutes = Router();
+
+const upload = multer(uploadConfig.MULTER);
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
@@ -14,5 +20,6 @@ foodsRoutes.get("/:id", foodsController.show);
 foodsRoutes.get("/", foodsController.index);
 foodsRoutes.delete("/:id", foodsController.delete);
 foodsRoutes.put("/:id", foodsController.update);
+foodsRoutes.patch("/avatar/:id", upload.single("avatar"), foodAvatarController.update)
 
 module.exports = foodsRoutes;
